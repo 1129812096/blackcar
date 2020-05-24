c43c9c3a61290a9141c587b2a20f7bfdea4665ce
1129812096 <1129812096@qq.com>
2020-05-24 14:04:33 +0800

    sad

---
+++ src/main/java/cn/openview/controller/PageController.java
@@ -0,0 +1,18 @@
+package cn.openview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping("/index.html")
+	public String pageindex() {
+		
+		
+		return "index";
+	}
+
+	
+}
